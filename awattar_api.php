899c5450b738eaa20311eb133c9012a33c5966cb
Franz27-js <franztsn27@gmail.com>
2025-05-27 14:10:54 +0200

    get api data

--- awattar_api.php
+++ awattar_api.php
@@ -1,7 +1,15 @@
 <?php
 
+$today_start = strtotime('today 00:00');
+$today_end = strtotime('today 23:59');
+
+$tomorrow_start = strtotime('tomorrow 00:00');
+$tomorrow_end = strtotime('tomorrow 23:59');
+
 // api url
-$api_url = 'https://api.awattar.at/v1/marketdata';
+$api_url = 'https://api.awattar.at/v1/marketdata?start='.$today_start.'000&end='.$tomorrow_end.'000';
+
+var_dump($api_url);
 
 // Initialize cURL
 $ch = curl_init();
@@ -25,7 +33,7 @@ curl_close($ch);
 // Check if request was successful
 if ($httpCode >= 200 && $httpCode < 300) {
   // Decode JSON response
-  $data = json_decode($response, true)['data'];
-  
+  $data = json_decode($response, true)['data'][0];
+
   var_dump($data);
 }
