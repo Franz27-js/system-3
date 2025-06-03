852884d6619dd51e0caa243431edfe196b5422ec
Franz27-js <franztsn27@gmail.com>
2025-06-03 13:30:32 +0200

    basic pricing data

--- awattar_api.php
+++ awattar_api.php
@@ -1,39 +1,83 @@
 <?php
 
-$today_start = strtotime('today 00:00');
-$today_end = strtotime('today 23:59');
+function get_api_data(): array {
+  $today_start = strtotime('today 00:00');
+  $today_end = strtotime('today 23:59');
 
-$tomorrow_start = strtotime('tomorrow 00:00');
-$tomorrow_end = strtotime('tomorrow 23:59');
+  $tomorrow_start = strtotime('tomorrow 00:00');
+  $tomorrow_end = strtotime('tomorrow 23:59');
 
-// api url
-$api_url = 'https://api.awattar.at/v1/marketdata?start='.$today_start.'000&end='.$tomorrow_end.'000';
+  // api url
+  $api_url = 'https://api.awattar.at/v1/marketdata?start='.$today_start.'000&end='.$today_end.'000';
 
-var_dump($api_url);
+  // Initialize cURL
+  $ch = curl_init();
 
-// Initialize cURL
-$ch = curl_init();
+  // Set cURL options
+  curl_setopt($ch, CURLOPT_URL, $api_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 
-// Set cURL options
-curl_setopt($ch, CURLOPT_URL, $api_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+  // Execute the request
+  $response = curl_exec($ch);
 
-// Execute the request
-$response = curl_exec($ch);
+  // Get HTTP status code
+  $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-// Get HTTP status code
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  // Close cURL
+  curl_close($ch);
 
-// Close cURL
-curl_close($ch);
+  // Check if request was successful
+  if ($httpCode >= 200 && $httpCode < 300) {
+    // Decode JSON response
+    return json_decode($response, true)['data'];
+  } else {
+    return [];
+  }
+}
 
-// Check if request was successful
-if ($httpCode >= 200 && $httpCode < 300) {
-  // Decode JSON response
-  $data = json_decode($response, true)['data'][0];
+function get_current_data(): array {
+  // api url
+  $api_url = 'https://api.awattar.at/v1/marketdata?start='.strtotime('now').'000';
 
-  var_dump($data);
+  // Initialize cURL
+  $ch = curl_init();
+
+  // Set cURL options
+  curl_setopt($ch, CURLOPT_URL, $api_url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+  // Execute the request
+  $response = curl_exec($ch);
+
+  // Get HTTP status code
+  $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+  // Close cURL
+  curl_close($ch);
+
+  // Check if request was successful
+  if ($httpCode >= 200 && $httpCode < 300) {
+    // Decode JSON response
+    return json_decode($response, true)['data'][0];
+  } else {
+    return [];
+  }
+}
+
+function get_best_price(): array {
+  $raw_data = get_api_data();
+
+  $price = array_column($raw_data, 'marketprice');
+
+  array_multisort($price, SORT_ASC, $raw_data);
+
+  $best_data = $raw_data[0];
+
+  return $best_data;
 }
--- data.php
+++ data.php
@@ -4,13 +4,31 @@ session_start();
 $_SESSION['page_title'] = 'Data';
 
 include_once 'assets/header.php';
+include_once 'awattar_api.php';
 
+$best_prices = get_best_price();
+$best_starting_time = substr($best_prices['start_timestamp'], 0, -3);
+$best_ending_time = substr($best_prices['end_timestamp'], 0, -3);
+$best_price = $best_prices['marketprice'];
+$best_unit = $best_prices['unit'];
+$best_date = date('d-m-Y H:i', $best_starting_time);
+
+$next_prices = get_current_data();
+$next_starting_time = substr($next_prices['start_timestamp'], 0, -3);
+$next_ending_time = substr($next_prices['end_timestamp'], 0, -3);
+$next_price = $next_prices['marketprice'];
+$next_unit = $next_prices['unit'];
+$next_date = date('d-m-Y H:i', $next_starting_time);
 ?>
 
 <div class="content_column">
   <div class="inner_content_container inner_content_50">
     <div class="pricing_container">
       <div class="inner_content_heading">Pricing:</div>
+      <div class="pricing_content data_price_container">Best Price: <?=$best_price?>€</div>
+      <div class="pricing_content">Date: <?=$best_date?></div>
+      <div class="pricing_content data_price_container">Next Price: <?=$next_price?>€</div>
+      <div class="pricing_content">Date: <?=$next_date?></div>
     </div>
   </div>
 </div>
